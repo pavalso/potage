56a99315a132c66f167bd4c6a5ad58b60a5a75e9
pavalso <92272697+pavalso@users.noreply.github.com>
2024-03-25 21:01:48 +0100

    Update proyect url

--- pypotage/shared.py
+++ pypotage/shared.py
@@ -1,7 +1,7 @@
 __proyect__ = "pypotage"
 
 __author__ = "Pavalso"
-__url__ = "https://github.com/pavalso/pypotage"
+__url__ = "https://github.com/pavalso/potage"
 
 __version__ = "0.0.0"
 __description__ = "Beans is a simple and lightweight Python library based on Spring Framework. It provides a simple way to manage beans and their dependencies."
