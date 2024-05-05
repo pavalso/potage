c79b6c436fef6fcd05be7c85c06929b6af9a491f
pavalso <92272697+pavalso@users.noreply.github.com>
2024-05-05 22:09:07 +0200

    Add favicon image

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'press'
 html_static_path = ['_static']
 html_css_files = ['potage_theme.css']
+html_favicon = 'images/favicon.ico'
 
 html_theme_options = {
     "project_name": "Pypotage",
