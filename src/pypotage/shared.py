cd82da3e1b1eaefbfada5b6a6e408ef6e491a9f2
pavalso <92272697+pavalso@users.noreply.github.com>
2024-12-08 11:40:50 +0100

    Prepare v2.0.0.1-alpha release

--- src/pypotage/shared.py
+++ src/pypotage/shared.py
@@ -3,7 +3,7 @@ __proyect__ = "pypotage"
 __author__ = "Pavalso"
 __url__ = "https://github.com/pavalso/potage"
 
-__version__ = "v2.0.0-alpha"
+__version__ = "v2.0.0.1-alpha"
 __description__ = """Pypotage is a simple Python package that provides an 
 easy way to use Dependency Injection in your projects."""
 __license__ = "MIT"
