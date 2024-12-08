401b60367d2ac65f136a6d160646856bd3a8e4d5
pavalso <92272697+pavalso@users.noreply.github.com>
2024-12-08 11:38:07 +0100

    Add __init__ to implementations dir

---
+++ src/pypotage/implementations/__init__.py
@@ -0,0 +1,14 @@
+from .chefLine import ChefLine
+from .ingredient import Ingredient
+from .kitchen import Kitchen
+from .meal import Meal
+from .pot import Pot
+
+
+__all__ = [
+    "ChefLine",
+    "Ingredient",
+    "Kitchen",
+    "Meal",
+    "Pot",
+]
